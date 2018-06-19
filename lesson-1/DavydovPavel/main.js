cc37dcce1e80851496cd53883b49e99fa29e28f8
Pavel <davidov.pavel@gmail.com>
2018-06-19 13:32:21 +0300

    homework-1

---
+++ lesson-1/DavydovPavel/main.js
@@ -0,0 +1,63 @@
+var listMenu = [
+    {
+        title: 'Животные',
+        items: [
+            {
+                title: 'Млекопитающие',
+                items: [
+                    { title: 'Коровы' },
+                    { title: 'Ослы' },
+                    { title: 'Собаки' },
+                    { title: 'Кошки', items: [{ title: 'Тигры' }, { title: 'Львы' }] },
+                ]
+            },
+            {
+                title: 'Другие',
+                items: [
+                    {
+                        title: 'Насекомые',
+                        items: [{ title: 'Членистоногие', items: [{ title: 'Тараканы' }] }]
+                    },
+                    { title: 'Змеи' },
+                    { title: 'Птицы' },
+                    { title: 'Ящерицы' },
+                ]
+            },
+        ]
+    },
+    {
+        title: 'Рыбы',
+        items: [
+            {
+                title: 'Аквариумные',
+                items: [{ title: 'Гуппи' }, { title: 'Скалярии' }]
+            },
+            {
+                title: 'Форель',
+                items: [{ title: 'Морская форель' }]
+            },
+        ]
+    },
+];
+function generateMenu(list) {
+    var content = '<ul>';
+    list.forEach(function (a) {
+        var flag = a.items !== undefined, templ = flag ? "<a class='title'>" + a.title + "</a>" + generateMenu(a.items) : "<a>" + a.title + "</a>";
+        content += "<li>" + templ + "</li>";
+    });
+    content += '</ul>';
+    return content;
+}
+var navMenuList = document.querySelector('.menu');
+if (navMenuList) {
+    navMenuList.innerHTML = generateMenu(listMenu);
+    navMenuList.onclick = function (e) {
+        var el = e.target;
+        var classList = el.classList;
+        if (!classList.contains('title')) {
+            return;
+        }
+        var parentLi = el.parentNode;
+        parentLi.classList.toggle('menu-open');
+    };
+}
