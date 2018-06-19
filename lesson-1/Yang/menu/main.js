c85e14143685588d063c78972bebcfa256cb7a71
Victor Yang <yanky2000@yahoo.com>
2018-06-20 00:49:46 +0300

    added HW1

---
+++ lesson-1/Yang/menu/main.js
@@ -0,0 +1,66 @@
+"use strict";
+var menu = [
+    {
+        title: 'Животные', items: [
+            {
+                title: 'Млекопитающие', items: [
+                    { title: 'Коровы' },
+                    { title: 'Ослы' },
+                    { title: 'Собаки' },
+                    { title: 'Тигры' }
+                ]
+            },
+            {
+                title: 'Другие', items: [
+                    { title: 'Змеи' },
+                    { title: 'Птицы' },
+                    { title: 'Ящерицы' },
+                ],
+            },
+        ]
+    },
+    {
+        title: 'Рыбы', items: [
+            {
+                title: 'Аквариумные', items: [
+                    { title: 'Гуппи' },
+                    { title: 'Скалярии' }
+                ]
+            },
+            {
+                title: 'Форель', items: [
+                    { title: 'Морская форель' }
+                ]
+            },
+        ]
+    }
+];
+function generateMenu(list, className) {
+    if (className === void 0) { className = 'title'; }
+    var content = "<ul>";
+    for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+        var a = list_1[_i];
+        content += "<li><a class=" + (a.items ? className : '') + ">" + a.title + "</a>";
+        if (a.items) {
+            content += generateMenu(a.items);
+        }
+        else {
+            content += "</li>";
+        }
+    }
+    content += "</ul>";
+    return content;
+}
+var div = document.querySelector('.menu');
+if (div) {
+    div.innerHTML = generateMenu(menu);
+    div.onclick = function (ev) {
+        var el = ev.target;
+        var classList = el.classList;
+        if (!classList.contains('title')) {
+            return;
+        }
+        var parentLi = el.parentNode;
+        parentLi.classList.toggle('menu-open');
+    };
+}
