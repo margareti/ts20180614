03434b5499d712f539f9fdecc5df67af31941ffb
kln4a <a.sirotkin@tradesoft.ru>
2018-06-20 23:03:23 +0300

    рендеринг меню

--- lesson-1/class-work/menu/main.js
+++ lesson-1/class-work/menu/main.js
@@ -1,33 +1,89 @@
-import data from './menuData';
-
-function renderDropList(list: string[]) {
-  list.for;
-}
-
-function generateMenu(list) {
-  var content = '<ul>';
-  for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
-    var a = list_1[_i];
-    content += "<li><a class='title'>" + a.title + '</a><ul>';
-    for (var _a = 0, _b = a.items; _a < _b.length; _a++) {
-      var item = _b[_a];
-      content += '<li><a>' + item + '</a></li>';
+"use strict";
+var menuData = [
+    {
+        title: 'Животные',
+        items: [
+            {
+                title: 'Млекопитающие',
+                items: [
+                    {
+                        title: 'Коровы'
+                    }, {
+                        title: 'Ослы'
+                    }, {
+                        title: 'Собаки'
+                    }, {
+                        title: 'Тигры'
+                    }
+                ]
+            }, {
+                title: 'Другие',
+                items: [
+                    {
+                        title: 'Змеи'
+                    }, {
+                        title: 'Птицы'
+                    }, {
+                        title: 'Ящерицы'
+                    }
+                ]
+            }
+        ]
+    }, {
+        title: 'Рыбы',
+        items: [
+            {
+                title: 'Аквариумные',
+                items: [
+                    {
+                        title: 'Гуппи'
+                    }, {
+                        title: 'Скалярии'
+                    }
+                ]
+            }, {
+                title: 'Форель',
+                items: [
+                    {
+                        title: 'Морская форель'
+                    }
+                ]
+            }
+        ]
     }
-    content += '</li></ul>';
-  }
-  content += '</ul>';
-  return content;
-}
+];
+var renderList = function (list) {
+    var ul = document.createElement('ul');
+    list.forEach(function (item) {
+        ul.appendChild(renderItem(item));
+    });
+    return ul;
+};
+var renderItem = function (item) {
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+    link.innerText = item.title;
+    li.appendChild(link);
+    if (item.hasOwnProperty('items') && Array.isArray(item.items)) {
+        li.appendChild(renderList(item.items));
+        link.addEventListener('click', function () {
+            li.classList.toggle('menu-open');
+        });
+    }
+    return li;
+};
 var navMenuList = document.querySelector('.menu');
 if (navMenuList) {
-  navMenuList.innerHTML = generateMenu(menuList);
-  navMenuList.onclick = function(ev) {
-    var el = ev.target;
-    var classList = el.classList;
-    if (!classList.contains('title')) {
-      return;
-    }
-    var parentLi = el.parentNode;
-    parentLi.classList.toggle('menu-open');
-  };
+    var rendered = renderList(menuData);
+    console.log(rendered);
+    navMenuList.appendChild(rendered);
+    // navMenuList.onclick = (ev: MouseEvent) => {
+    //     const el: HTMLAnchorElement = ev.target as HTMLAnchorElement;
+    //     const classList = el.classList;
+    //     if (!classList.contains('title')) {
+    //         return;
+    //     }
+    //     const parentLi = el.parentNode as HTMLLIElement;
+    //     parentLi.classList.toggle('menu-open');
+    // };
 }
