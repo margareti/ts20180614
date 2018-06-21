387477583a1b8b3709a6f0a5e0fac8f5488e2997
Aleksandr Gerasimov <alexandr.gerasimov@iqoption.com>
2018-06-21 12:28:53 +0300

    gerasimov-lesson-1 add homework  ts lesson 1

---
+++ lesson-1/GerasimovAleksandr/index.js
@@ -0,0 +1,62 @@
+const menuList = [
+    {
+        title: "Животные",
+        items: [
+            {
+                title: "Млекопитающие",
+                items: [
+                    { title: "Коровы" },
+                    { title: "Ослы" },
+                    { title: "Собаки" },
+                    { title: "Тигры" }
+                ]
+            },
+            {
+                title: "Другие",
+                items: [{ title: "Змеи" }, { title: "Птицы" }, { title: "Ящерицы" }]
+            }
+        ]
+    },
+    {
+        title: "Рыбы",
+        items: [
+            {
+                title: "Аквариумные",
+                items: [{ title: "Гуппи" }, { title: "Скалярии" }]
+            },
+            {
+                title: "Форель",
+                items: [{ title: "Морская форель" }]
+            }
+        ]
+    }
+];
+function generateMenu(list) {
+    let content = `<ul>`;
+    for (const menuItem of list) {
+        content += "<li>";
+        if ((menuItem.items && menuItem.items.length > 0) || false) {
+            content += `<a class="title">${menuItem.title}</a>`;
+            content += generateMenu(menuItem.items);
+        }
+        else {
+            content += `<a>${menuItem.title}</a>`;
+        }
+        content += `</li>`;
+    }
+    content += `</ul>`;
+    return content;
+}
+let navMenuList = document.querySelector(".menu");
+if (navMenuList) {
+    navMenuList.innerHTML = generateMenu(menuList);
+    navMenuList.onclick = (ev) => {
+        const el = ev.target;
+        const classList = el.classList;
+        if (!classList.contains("title")) {
+            return;
+        }
+        const parentLi = el.parentNode;
+        parentLi.classList.toggle("menu-open");
+    };
+}
